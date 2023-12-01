7f5115027132755e2b93a96f967982a9543d7650
Varunshiremat107 <152603676+Varunshiremat107@users.noreply.github.com>
2023-12-01 23:03:19 +0530

    Update process3.c

--- process3.c
+++ process3.c
@@ -1,217 +1,77 @@
-#include<stdio.h>
-#include<unistd.h>
-#include<fcntl.h>
-#include<stdlib.h>
-#include<string.h>
-#include<sys/shm.h>
-#include<sys/ipc.h>
-#include<pthread.h>
-#include<sys/msg.h>
-#include<sys/types.h>
-#define MAX_TEXT 1024
-FILE *fp;
+#include <fcntl.h>
+#include <semaphore.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <unistd.h>
 
-struct enviro
-{
-	int noise;
-	int air;
-	char weather[10];
+struct enviro {
+  int noise;
+  int air;
+  char weather[10];
 };
-struct trf
-{
-	char cam1[10];
-	char cam2[10];
-	char cam3[10];
+struct trf {
+  char cam1[10];
+  char cam2[10];
+  char cam3[10];
 };
 
- struct  scity{
- struct enviro e1;
- struct trf t1;
- int  waste;
+struct scity {
+  struct enviro e1;
+  struct trf t1;
+  int waste;
 };
 
-struct mq
+
+void noise_entry(struct scity *sm)
 {
-	char b1[7][100];
-	
-};
-struct mq data;
-
-
-
-void* trafficfunc()
-{
-		
-	
-	int shmid;
-	struct scity *sm;
-	shmid = shmget((key_t)1234,1024,0666);
-	fp=fopen("output.txt","a");
-	sm =(struct scity *)shmat(shmid, NULL, 0);
-	if(strcmp(sm->t1.cam1,"low")==0)
-	{
-		fprintf(fp,"Route 1 is clear ur good to go\n");
-
-		
-	}
-	else 
-	{
-		
-		
-		fprintf(fp,"Route 1 is high in traffic better avoid\n");
-		
-	}
-	if(strcmp(sm->t1.cam2,"low")==0)
-        {
-               
-		fprintf(fp,"Route 2 is clear ur good to go\n");
-        }
-        else
-        {
-                
-		fprintf(fp,"Route 2 is high in traffic better avoid\n");
-		
-        }
-        
-	if(strcmp(sm->t1.cam3,"low")==0)
-        {
-                
-		fprintf(fp,"Route 3 is clear ur good to go\n");
-		
-		
-        }
-        else
-        {
-                
-		fprintf(fp,"Route 1 is high in traffic better avoid\n");
-		
-        }
-        fclose(fp);
-
-
-	return NULL;
+  printf("enter the noise in DB: \n");
+  scanf("%d", &sm->e1.noise);
 }
 
-void* wastefunc()
+void air_entry(struct scity *sm) 
 {
-
-	
-	int shmid;
-	struct scity *sm;
-	shmid = shmget((key_t)1234,1024,0666);
-	fp=fopen("output.txt","a");
-	sm =(struct scity *)shmat(shmid, NULL, 0);
-	int waste=sm->waste;
-	char buff[40];
-	sprintf(buff,"the amount of waste is %d\n",waste);
-	fprintf(fp,"%s\n",buff);
-	fclose(fp);
-	return NULL;
-	
+  printf("enter the air in DB: \n");
+  scanf("%d", &sm->e1.air);
 }
-void* envfunc()
-{
-		
-	
-	int shmid;
-	struct scity *sm;
-	shmid = shmget((key_t)1234,1024,0666);
-	sm =(struct scity *)shmat(shmid, NULL, 0);
-	fp=fopen("output.txt","a");
-	if(sm->e1.noise >=0 && sm->e1.noise <=50)
-	{
-		
-		fprintf(fp,"Noise is in the low range \n");
-		
-		
-	}
-	else if(sm->e1.noise >=50 && sm->e1.noise<=100)
-	{
-		
-		fprintf(fp,"Noise id the medium range \n");
-		
-	}
-	else 
-	{
-		
-		fprintf(fp,"TOO noisy outside be aware\n");
-		
-	}
-	if(sm->e1.air >=0 && sm->e1.air<=200)
-	{
-		
-		fprintf(fp,"Air quality is good u can go \n");
-		
-		
-	}
-	else if(sm->e1.air >=200 && sm->e1.air<=350)
-	{
-		
-		fprintf(fp,"Air quality is quite bad better wear a mask \n");
-		
-	}
-	else
-	{
-		
-		fprintf(fp,"Air quality is too bad be alert\n");
-		
-	}
-	if(strcmp(sm->e1.weather,"rainy")==0)
-	{
-		
-		fprintf(fp,"Its Rainy outside take ur umbrella\n");
-		
-		
-	}
-	else if(strcmp(sm->e1.weather,"cloudy")==0)
-        {
-                
-               fprintf(fp,"Its cloudy outside wait for sometime  \n");
-		
-        }
-	else 
-		
-        {
-                
-                fprintf(fp,"Its clear  outside u can go out \n");
-		
-        }
-        fclose(fp);
-	return NULL;
 
+void weather_entry(struct scity *sm)
+{
+  char weat1[10];
+  printf("Enter the Weather condition : \n");
+  scanf("%s", weat1);
+  int len2 = strlen(weat1);
+
+  strcpy((sm->e1.weather), weat1);
 }
-		
 
 
 
 
-int main()
-{
+int main() {
+
   struct scity *sm;
   int shmid;
-  pthread_t t1,t2,t3;
-  
-  
-  shmid = shmget((key_t)1234,1024,0666);
-  printf("key of the shared memory is : %d\n",shmid); 
-  sm =(struct scity *)shmat(shmid, NULL, 0);
-  //printf("camera 1 %s\n",sm->t1.cam1);
-  //printf("camera 2%s\n",sm->t1.cam2);
-  //printf("camera 2 %s\n",sm->t1.cam3);
-  //printf("waste %d\n",sm->waste );
-  //printf("noise %d\n",sm->e1.noise);
-  //printf("air %d\n",sm->e1.air);
-  //printf("weather %s\n",sm->e1.weather);
+  int val;
+  sem_t *semaphore;
+  semaphore = sem_open("/semap1", O_RDWR);
+  //char air1[10];
+
+  shmid = shmget((key_t)1234, 1024, 0666);
+  printf("key of the shared memory is : %d\n", shmid);
+  sm = (struct scity *)shmat(shmid, NULL, 0);
+  sem_getvalue(semaphore, &val);
+  printf("value before %d\n", val);
+  sem_wait(semaphore);
+  sem_getvalue(semaphore, &val);
 
 
-  pthread_create (&t1,NULL,trafficfunc,NULL);
-  pthread_join(t1,NULL);
-  pthread_create (&t2,NULL,wastefunc,NULL);
-  pthread_join(t2,NULL);
-  pthread_create (&t3,NULL,envfunc,NULL);
-  pthread_join(t3,NULL);
-  
-  
-  
-  }
-  
+   noise_entry(sm);
+   air_entry(sm);
+    weather_entry(sm);
+
+  sem_post(semaphore);
+  printf("value after %d\n", val);
+}
