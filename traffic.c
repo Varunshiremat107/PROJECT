d24562b6bf39b9446b8944049e36677c67255c9f
Varunshiremat107 <152603676+Varunshiremat107@users.noreply.github.com>
2023-12-03 10:37:01 +0530

    Update traffic.c

--- traffic.c
+++ traffic.c
@@ -7,87 +7,54 @@
 #include <stdlib.h>    // standard library functions
 #include <string.h>    // string manipulation functions
 #include <sys/shm.h>   // shared memory
+#include <unistd.h>
 #include "structure.h"
-
+#include <time.h>
 void error_handler(char *message)
 {
        	perror(message);
 }
+int random_input(int min ,int max)
+{
+       	srand(time(0));
+       	return rand() %(max -min+1)+min;
+}
 
 // Function to capture traffic condition from camera 1
 
-void *camera1(void *arg)
+void camera1(void *arg)
 {
 
-	char buff[5];
+	int camera1;
 	struct scity *sm = (struct scity *)arg;
-      	printf("Enter the traffic condition in camera 1 : \n");
-	printf("High or Low\n");
-	scanf("%s", buff);
-
-
-      	// error handling
- 
-       	if (strcmp(buff, "High") == 0) {
-		strcpy(sm->t1.cam1, buff);
-		pthread_exit(0);	
-	}
-       	else if (strcmp(buff, "Low") == 0) {
-	       	strcpy(sm->t1.cam1, buff);
-		pthread_exit(0);
-       	}
-       	else {
-   
-	       	error_handler("Invalid Input\n");
-	       	exit(0);
-       	}
+	camera1=random_input(1,100);
+	sm->t1.cam1=camera1;
+      	printf(" traffic sensed  in camera 1 : %d\n",sm->t1.cam1);
+	
+       	
 }
 
 
 // Function to capture traffic condition from camera 2
 
-void *camera2(void *arg) {
-	char buff[5];
+void camera2(void *arg) {
+	int camera2;
        	struct scity *sm = (struct scity *)arg;
-       	printf("Enter the traffic condition in camera 2 : \n");
-       	scanf("%s", buff);
-	 // error handling
-	 if (strcmp(buff, "High") == 0) {
-		 strcpy(sm->t1.cam2, buff);
-		 pthread_exit(0);
-	 }
-	 else if (strcmp(buff, "Low") == 0) {
-		 strcpy(sm->t1.cam2, buff);
-		 pthread_exit(0);
-	 }
-	 else {
-		 error_handler("Invalid Input\n");
-		 strcpy(sm->t1.cam2, buff);
-		 pthread_exit(0);
-	 }
+       	camera2=random_input(1,100);
+	sm->t1.cam2=camera2;
+      	printf(" traffic sensed  in camera 2 : %d\n",sm->t1.cam2);
+	
 }
 
 // Function to capture traffic condition from camera 3
-void *camera3(void *arg) {
-       	char buff[5];
+void camera3(void *arg) {
+        int camera3;
        	struct scity *sm = (struct scity *)arg;
-       	printf("Enter the traffic condition in camera 3 : \n");
-       	scanf("%s", buff);
-       
-	if (strcmp(buff, "High") == 0) {
-	       	strcpy(sm->t1.cam3, buff);
-	       	pthread_exit(0);
-       	}
-       	else if (strcmp(buff, "Low") == 0) {
-	       	strcpy(sm->t1.cam3, buff);
-	       	pthread_exit(0);
-       	}
-       	else {
-   
-	       	error_handler("Invalid Input\n");
-	       	strcpy(sm->t1.cam3, buff);
-	       	pthread_exit(0);
-       	}
+        camera3=random_input(1,100);
+	sm->t1.cam3=camera3;
+      	printf(" traffic sensed  in camera 3 : %d\n",sm->t1.cam3);
+	
+       	
 }
 
 
@@ -109,14 +76,17 @@ int main() {
 	   shmid = shmget((key_t)1234, sizeof(struct scity), 0666);
 	   sm = (struct scity *)shmat(shmid, NULL, 0);
 	   sem_wait(semaphore); // Dececreasing the value of semaphore by 1
+	   
+	   printf("\n............Traffic Process............\n");
 
 	   // create threads for each camera function and join them to the main thread.
 	   pthread_create(&tid1, NULL, camera1, (void *)sm); // API
            pthread_join(tid1, NULL);
 	   pthread_create(&tid2, NULL, camera2, (void *)sm);
-	   pthread_jiioin(tid2, NULL);
-	   pthread_creiiate(&tid3, NULL, camera3, (void *)sm);
+	   pthread_join(tid2, NULL);
+	   pthread_create(&tid3, NULL, camera3, (void *)sm);
 	   pthread_join(tid3, NULL);
 	 
 	   sem_post(semaphore); // Incrementing the value of semaphore by 1
+	   execl("./p2","p2",NULL,NULL);
 }
