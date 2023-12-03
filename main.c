4a6d4bd9742c0a6da8094dc6050b9b3df143ad24
RitheshTD <152706630+RitheshTD@users.noreply.github.com>
2023-12-03 20:49:51 +0530

    Update main.c

--- main.c
+++ main.c
@@ -6,47 +6,35 @@
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <unistd.h>
-
-// semaphore.h
-
-struct enviro {
-  int noise;
-  int air;
-  char weather[10];
-};
-
-struct trf {
-  char cam1[10];
-  char cam2[10];
-  char cam3[10];
-};
-
-struct scity {
-  struct enviro e1;
-  struct trf t1;
-  int waste;
-};
-
-int main() {
-
+#include "structure.h"
+int main() 
+{
   sem_t *semaphore;
-
   semaphore = sem_open("/semap1", O_CREAT, 0600, 1);
-  if (semaphore == SEM_FAILED) {
+  if (semaphore == SEM_FAILED) 
+  {
     perror("sem_open failed");
     exit(EXIT_FAILURE);
   }
-
   struct scity *sm;
   int shmid;
-
   struct scity *p = (struct scity *)malloc(sizeof(struct scity));
   shmid = shmget((key_t)1234, 1024, 0666 | IPC_CREAT);
-  if (shmid == -1) {
+  if (shmid == -1) 
+  {
     perror("shmget");
   }
-  printf("key of shared memory is %d\n", shmid);
-
+  else{
+  printf("\n:........EMPTY STRUCTURE IN THE SHARED MEMORY CREATED........:\n");
+  }
+  printf("\n:...................KEY of shared memory.............:=> %d\n", shmid);
   sm = (struct scity *)shmat(shmid, NULL, 0);
+  system("gcc Traffic.c -o p1");
+  system("gcc Wastage.c -o p2");
+  system("gcc Environment.c -o p3");
   
+  system("gcc decision.c -o p4");
+  system("gcc output.c -o p5");
+  sleep(5);
+  execl("./p1","p1",NULL,NULL);
 }
