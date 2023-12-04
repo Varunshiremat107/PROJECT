f36f4013443a6c8386c2d28b4b998d0f00991381
SoumyaBandigani <152683763+SoumyaBandigani@users.noreply.github.com>
2023-12-04 17:32:18 +0530

    Update Environment.c

--- Environment.c
+++ Environment.c
@@ -1,18 +1,14 @@
 #include "headerfile.h"
 #include "structure.h"
-
 int Random_Input(unsigned int min ,unsigned int max)
 {
        	srand(time(0));
 	return rand() %(max -min+1)+min;
 }
-
 void Error_Handler(char *message) {
        	perror(message);
 }
-
 // Sensor input of Noise
-
 void *Noise_Condition(void *arg) 
 {
        	struct scity *sm = (struct scity *)arg;
@@ -22,9 +18,7 @@ void *Noise_Condition(void *arg)
        	printf("\n:........Noise  Quality Sensed by Noise Senser........:=> %d\n",sm->e1.noise);
        	return NULL;
 }
-
 // Entering the air condition input
-
 void *Air_Condition(void *arg) {
        	struct scity *sm = (struct scity *)arg;
        	unsigned int Input2;
@@ -33,9 +27,7 @@ void *Air_Condition(void *arg) {
        	printf("\n:........Air Quality Sensed by Air Senser........:=> %d\n",sm->e1.air);
        	return NULL;
 }
-
 // Entering the weather input
-
 void *Weather_Condtion(void *arg) {
        	struct scity *sm = (struct scity *)arg;
        	unsigned int choice ;
@@ -61,59 +53,50 @@ void *Weather_Condtion(void *arg) {
 	}
 	return NULL;
 }
-
 // storing data in file for backup
-
-void File_Store(struct scity *sm) {
+void File_Store(struct scity *sm)
+{
        	FILE *fp;
        	fp = fopen("data1.txt", "a");
-       
-	if (fp == NULL) {
+        if (fp == NULL)
+	{
 	       	perror("ERROR OPEING FILE");
 	       	exit(1);
 	}
-
-       	fprintf(fp, "noise:%d  \n", sm->e1.noise);
+	fprintf(fp, "noise:%d  \n", sm->e1.noise);
        	fprintf(fp, "air:%d  \n", sm->e1.air);
        	fprintf(fp, "weather:%s  \n", sm->e1.weather);
        	fclose(fp);
 }
 
-int main() {
-
-        printf("\n............Environment Process............\n");
-
-       	pthread_t tid1, tid2, tid3;
+int main()
+{
+	printf("\n............Environment Process............\n");
+	pthread_t tid1, tid2, tid3;
        	struct scity *sm; // structure pointer
         int shmid;        // shared memory id
         sem_t *semaphore;                        // semaphore pointer
         semaphore = sem_open("/semap1", O_RDWR); // opening semaphore
-						 
-        if (semaphore == SEM_FAILED) {
+	if (semaphore == SEM_FAILED)
+	{
 	       	Error_Handler("semaphore initialization failed");
        	}
-       
-	shmid = shmget((key_t)1234, 1024, 0666); // opening shared memory
-        if (shmid == -1) {
+       shmid = shmget((key_t)1234, 1024, 0666); // opening shared memory
+        if (shmid == -1) 
+	{
 	       	Error_Handler("shmget failed");
        	}
        	printf("key of the shared memory is : %d\n", shmid);
        	sm = (struct scity *)shmat(shmid, NULL,0); // attaching shared memory to the process
-
-       	sem_wait(semaphore); // waiting for semaphore
+	sem_wait(semaphore); // waiting for semaphore
        	pthread_create(&tid1, NULL, Noise_Condition, (void *)sm); // API
        	pthread_join(tid1, NULL);
        	pthread_create(&tid2, NULL, Air_Condition, (void *)sm);
- 
-       	pthread_join(tid2, NULL);
+ 	pthread_join(tid2, NULL);
 	pthread_create(&tid3, NULL, Weather_Condtion, (void *)sm);
 	pthread_join(tid3, NULL);
-
 	sem_post(semaphore); // signaling semaphore
-	
-
 	File_Store(sm);
 	sleep(5);
 	execl("./p4","p4",NULL,NULL);
-	
 }
