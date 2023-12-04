3c91463c7366c3a5d7156c18a987dd4a74ac97b1
RitheshTD <152706630+RitheshTD@users.noreply.github.com>
2023-12-04 17:50:52 +0530

    Update output.c

--- output.c
+++ output.c
@@ -1,26 +1,25 @@
 #include "headerfile.h"
 #define FILENAME "file.txt"
-
 // declaring structure variable
-struct mq {
+struct mq 
+{
   char b1[7][100];
 };
-
-int main() {
+int main()
+{
   FILE *file = fopen(FILENAME, "a");
-  if (file == NULL) {
+  if (file == NULL)
+  {
     perror("Error opening file");
     exit(EXIT_FAILURE);
   }
-
-  int msgid;
-  char buff1[50];
-
-  struct mq data;
-  printf("\n---------------------------------------------------\n");
-  // loop to print messages from message-Queue
-  for (int i = 0; i < 7; i++) 
-  {
+int msgid;
+char buff1[50];
+struct mq data;
+printf("\n---------------------------------------------------\n");
+// loop to print messages from message-Queue
+for (int i = 0; i < 7; i++) 
+{
     msgid = msgget((key_t)17834, 0666 | IPC_CREAT);
     msgrcv(msgid, &data, sizeof(data.b1), 0, 0);
     printf("\n%s\n", data.b1[i]);
